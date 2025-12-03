7474f612f0e266e4a1f47bdaaa1d85937a4710ff
LouisLP <louis.lascelles.palys@gmail.com>
2025-12-03 16:07:10 +0100

    Base-level footer and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { Icon } from '@iconify/vue'
+
+import Footer from '@/components/ui/BaseFooter.vue'
+import Navbar from '@/components/ui/BaseNavbar.vue'
 </script>
 
 <template>
+  <Navbar />
+
+  <main>
+    <RouterView />
+  </main>
+
   <div class="space-y-6">
     <!-- Tailwind -->
     <h1 class="text-3xl font-bold underline">
@@ -17,4 +26,6 @@ import { Icon } from '@iconify/vue'
     <!-- Iconify -->
     <Icon icon="mdi:home" class="size-6 text-gray-700" />
   </div>
+
+  <Footer />
 </template>
---
+++ src/components/ui/BaseNavbar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <div class="navbar bg-neutral shadow-sm">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <!-- Cart -->
+      <button tabindex="0" role="button" class="btn btn-ghost">
+        <Icon icon="mdi:cart" class="size-6 text-white" />
+        <!-- TODO: num of cart items -->
+        <div class="indicator-item ml-1">
+          7 items
+        </div>
+      </button>
+    </div>
+  </div>
+</template>
