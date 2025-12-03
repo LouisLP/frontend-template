ec5bf8035f18e08d9fcc164db047cf706f5de50a
LouisLP <louis.lascelles.palys@gmail.com>
2025-12-03 16:07:01 +0100

    Installing router

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+// import { createMemoryHistory, createRouter } from 'vue-router'
+
+// import DashboardView from './DashboardView.vue'
+// import HomeView from './HomeView.vue'
+
+// const routes = [
+//   { path: '/', component: HomeView },
+//   { path: '/dashboard', component: DashboardView },
+// ]
+
+// export const router = createRouter({
+//   history: createMemoryHistory(),
+//   routes,
+// })
