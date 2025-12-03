8dfc25580477ceb92dc0dbe4cdd285a25c48fa08
LouisLP <louis.lascelles.palys@gmail.com>
2025-12-03 15:33:01 +0100

    Default DaisyUI theme (and making sure it works)

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
   <h1 class="text-4xl font-bold underline">
     Hello world!
   </h1>
+
+  <button class="btn btn-outline btn-secondary">
+    Secondary
+  </button>
 </template>
 
 <style scoped></style>
